b14f6863f82ed01608b06f5294b08a8b00fc80e4
Derek Gustafson <degustaf@gmail.com>
2016-02-19 05:34:12 +0000

    Added lite linting

--- adventure/game_state.py
+++ adventure/game_state.py
@@ -1,6 +1,9 @@
 """ This module tracks the current status of the game """
 
 class State(object):
+    # pylint: disable=too-few-public-methods
+    # this is to temporarily disable this warning.  It should be removed once
+    # there are 2 public methods.
     """ This class encapsulates the current status of the game """
     def __init__(self):
         self.turn = 0
