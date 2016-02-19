c6e44fc814e9d1c29be1e9c85957a4e7b1178ea8
Derek Gustafson <degustaf@gmail.com>
2016-02-19 05:40:01 +0000

    Make code Flake8 compliant.  Adjust lite settings

--- adventure/location.py
+++ adventure/location.py
@@ -1,8 +1,5 @@
 
 class Location(object):
-    # pylint: disable=too-few-public-methods
-    # this is to temporarily disable this warning.  It should be removed once
-    # there are 2 public methods.
     """This class encapsulates location information and behavior"""
     def __init__(self, name="Room"):
         self.name = name
